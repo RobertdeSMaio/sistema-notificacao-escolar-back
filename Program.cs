d5d5f2b86129ac6a9538c10ffd6521e613eafc36
Robert de S. Maio <robertmaio@outlook.com>
2026-01-31 01:37:01 -0300

    adjust program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,27 @@
-using Microsoft.EntityFrameworkCore;
-using sistema_notificacao_escolar_back.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Configuração do Banco de Dados
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MinhaPoliticaReact", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") 
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// 2. Adicione suporte aos Controllers (Essencial para ver suas rotas!)
 builder.Services.AddControllers();
-
-// 3. Configuração do Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
-// --- AQUI O APP É CONSTRUÍDO ---
 var app = builder.Build();
 
-// 4. Configuração do Pipeline (O que acontece quando o app roda)
 if (app.Environment.IsDevelopment())
 {
-    // Ativa a interface visual do Swagger
     app.UseSwagger();
     app.UseSwaggerUI();
     app.MapOpenApi();
@@ -29,7 +29,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// 5. Mapeia os Controllers para que as rotas funcionem
+app.UseCors("MinhaPoliticaReact");
+
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
