11f46fe328f8d5669174350f7a156edc11c986c2
Robert de S. Maio <robertmaio@outlook.com>
2026-02-09 19:24:24 -0300

    fix swagger api

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
