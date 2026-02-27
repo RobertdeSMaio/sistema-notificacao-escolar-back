350f45ef5d4e48f4c4576d0eb84942555e0e197d
Robert de S. Maio <robertmaio@outlook.com>
2026-02-27 12:15:34 -0300

    .env error

--- obj/Debug/net10.0/sistema-notificacao-escolar-back.AssemblyInfo.cs
+++ obj/Debug/net10.0/sistema-notificacao-escolar-back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema-notificacao-escolar-back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2778a852a0dd1bede5ddaf85cf744a1a43d8ee05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f4f94079b0f03ab901d78d4b94175bbb79a0128")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema-notificacao-escolar-back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema-notificacao-escolar-back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
